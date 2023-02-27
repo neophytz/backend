e889cb74ea6a5e6e232a44a834dd0d3bb3967e2a
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-02-27 23:09:21 +0530

    Backend tutorial 2

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ const app = express(); // for initialising an express app
 
 app.use(bodyParser.json()); // for parsing all output in json
 
-app.use(cors("*"));
+app.use(cors("*")); // restrictions on accessibility of my endpoints
 
 const port = 8080; // 3000 -> react and 8080 -> backend
 
 app.get('/api', (req, res) => {
     console.log(req.body);
-    res.send({
-        "status":"working",
-    });
+    if(req.body.username==='nikhil' && req.body.password==='password'){
+        return res.send("Hello Nikhil, you are authorized user!!!");
+    }
+    else{
+        return res.send("You are not authorized!!");
+    }
+})
+
+// access tokens are response.
+// access denied. empty access token.
+
+// I can create a get request without any payload with the help of browser.
+
+app.post('/api', (req, res)=>{
+    return res.send("hello from backend!!");
 })
 
 app.get('/api/user', (req, res) => {
@@ -25,5 +37,5 @@ app.get('/api/user', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`App is running on http://localhost:${port}`);
+    console.log(`App is running on http://localhost:${port}/api`);
 });
