fe708b04dc97e1c4cba11807fbcb5fe4cfd2228f
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-03-04 23:01:21 +0530

    mvc architecture started.

--- shop-api/index.js
+++ shop-api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-var products = require('./store');
+const productRouter = require('./src/routes/products.routes.js')
 
 const app = express();
 
@@ -15,83 +15,7 @@ app.get('/', (req, res) => {
     })
 })
 
-// Products
-// getting all products
-
-app.get('/products/all', (req, res) => {
-    return res.send({
-        message:"All the products are being displayed.",
-        products:products,
-        count:products.length
-    })
-})
-
-// getting a single product 
-// get the product id from user.
-// find the product -> return the product otherwise I will return no product found.
-
-app.get('/products/get/:id', (req, res) => {
-    // find the id that i am getting from the user.
-    // query params -> helps us to make our urls dynamic.
-    // we can't send payload with the browser.
-    // we can send data in the endpoint.
-    var id = req.params.id; //(baseurl/endpoint/${id})
-    //var id = req.query.id;/// (baseurl/endpoint/?id=102)
-    // console.log(id);
-    // console.log(typeof(id));
-    var idx = products.findIndex(i => i.productId === parseInt(id));
-    if(idx!==-1){
-        return res.send({
-            message:"Product Found!",
-            product:products[idx]
-        });
-    }
-    else{
-        return res.send({
-            message:"No Product Found!"
-        });
-    }
-})
-
-//create new product
-app.post('/products/create', (req, res) => {
-    // adding product in products store.
-    // if my product id already exits? -> error
-    // done productName, productPrice, stock -> should be there -> empty , undefined -> error
-    if(products.findIndex(i => i.productId===req.body.productId)===-1){        
-        // productId is new. -> generate a new product.
-        products=[...products, req.body];
-        // sending the response back.
-        return res.send({
-            message:"Successfully Created New Product!!"
-        })
-    }
-    return res.send({
-        message:"Error While Generating New Product."
-    })
-})
-
-// deleting a product
-// find the product using product id.
-// if found -> delete it using array methods..
-// otherwise -> send an error response.
-app.delete('/products/delete', (req, res) => {
-    var id = req.query.id;
-    var idx = products.findIndex(i => i.productId === parseInt(id))
-    if( idx !== -1){
-        //delete the item
-        var item = products[idx];
-        products.splice(idx, 1);
-        // return res
-        return res.send({
-            message:"Successfully deleted the product.",
-            deletedProductDetails: item
-        })
-    }
-    return res.send({
-        message:"No such product found."
-    })
-})
+app.use('/products', productRouter);
 
 const PORT = process.env.PORT || 8080;
 
---
+++ shop-api/src/controllers/products.controller.js
@@ -0,0 +1,38 @@
+var products = require('../../store');
+const { http_formatter } = require("../util/formatter");
+
+const getAll = (req, res) => {
+    return res.send(http_formatter("All the products are being displayed",products));
+}
+
+const getSingleProduct = (req, res) => {
+    const id = req.params.id;
+    var idx = products.findIndex(i => i.productId === parseInt(id));
+    if(idx!==-1){
+        return res.send(http_formatter("Product Found", products[idx]))
+    }
+    else{
+        return res.send(http_formatter("No Product Found!"))
+    }
+}
+
+const createProduct = (req, res) => {
+    if(products.findIndex(i => i.productId===req.body.productId)===-1){        
+        products=[...products, req.body];
+        return res.send(http_formatter("Successfully created new product!!"))
+    }
+    return res.send(http_formatter("error while generating new product"))
+}
+
+const deleteProduct = (req, res) => {
+    var id = req.query.id;
+    var idx = products.findIndex(i => i.productId === parseInt(id))
+    if( idx !== -1){
+        var item = products[idx];
+        products.splice(idx, 1);
+        return res.send(http_formatter("Successfully deleted the product", item))
+    }
+    return res.send(http_formatter("No such product found"))
+}
+
+module.exports = {getAll, getSingleProduct, createProduct, deleteProduct}
