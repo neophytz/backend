13e278607b3b2a1381d581c4a3484a366520e8de
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-03-09 22:47:41 +0530

    find and delete queries explained

--- shop-api/src/controllers/products.controller.js
+++ shop-api/src/controllers/products.controller.js
@@ -2,19 +2,44 @@ var products = require('../../store');
 const { http_formatter } = require("../util/formatter");
 const { Product } = require('../schemas/product.schema')
 
-const getAll = (req, res) => {
-    return res.send(http_formatter("All the products are being displayed", products));
+//either you use async-await or you use then-catch
+
+// method 1
+// const getAll = async (req, res) => {
+    
+//     // const cursor = Product.find().cursor();
+
+//     // // order -> preparing food.
+//     // // eat -> consumed
+//     // // pay for the stuff.
+//     // var data = [];
+//     // for(let doc = await cursor.next(); doc!=null ; doc = await cursor.next()){
+//     //     data= [...data, doc];
+//     // }
+//     return res.send(http_formatter("All the products are being displayed", data));
+// }
+
+//method 2
+const getAll = (req, res) => { 
+    Product.find({}).select('name price').then((result)=> {
+        res.send(http_formatter("All products are being displayed", result))
+    }).catch((err) => res.send(http_formatter("Error while finding products", err)));
 }
 
 const getSingleProduct = (req, res) => {
     const id = req.params.id;
-    var idx = products.findIndex(i => i.productId === parseInt(id));
-    if(idx!==-1){
-        return res.send(http_formatter("Product Found", products[idx]))
-    }
-    else{
-        return res.send(http_formatter("No Product Found!"))
-    }
+    Product.find({_id:id}).then((result)=>{
+        res.send(http_formatter("Product Found", result))
+    }).catch((err) => {res.send(http_formatter("error while finding product", err))});
+
+    // const id = req.params.id;
+    // var idx = products.findIndex(i => i.productId === parseInt(id));
+    // if(idx!==-1){
+    //     return res.send(http_formatter("Product Found", products[idx]))
+    // }
+    // else{
+    //     return res.send(http_formatter("No Product Found!"))
+    // }
 }
 
 const createProduct = (req, res) => {
@@ -33,13 +58,16 @@ const createProduct = (req, res) => {
 
 const deleteProduct = (req, res) => {
     var id = req.query.id;
-    var idx = products.findIndex(i => i.productId === parseInt(id))
-    if( idx !== -1){
-        var item = products[idx];
-        products.splice(idx, 1);
-        return res.send(http_formatter("Successfully deleted the product", item))
-    }
-    return res.send(http_formatter("No such product found"))
+    Product.deleteOne({_id:id}).then((result) => {
+        res.send(http_formatter("successfully deleted", result))
+    }).catch((err)=> res.send(http_formatter("error while deleting product", err)));
+    // var idx = products.findIndex(i => i.productId === parseInt(id))
+    // if( idx !== -1){
+    //     var item = products[idx];
+    //     products.splice(idx, 1);
+    //     return res.send(http_formatter("Successfully deleted the product", item))
+    // }
+    // return res.send(http_formatter("No such product found"))
 }
 
 module.exports = {getAll, getSingleProduct, createProduct, deleteProduct}
