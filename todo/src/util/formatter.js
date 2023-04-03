ee613c2d37bdb9902297555886bc8f4074bb5432
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2023-04-03 22:57:03 +0530

    formatter updated

--- todo/src/util/formatter.js
+++ todo/src/util/formatter.js
@@ -1,3 +1,9 @@
 exports.http_formatter = (message, data=null, success=true) => {
+    if(success===false && data.code === 11000){
+        message = '';
+        Object.keys(data.keyValue).forEach(key => {
+            message += `${key} : ${data.keyValue[key]} already exists in our record.`
+        })
+    }
     return {message, data, success};
 }
